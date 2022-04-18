64a3b976915df18e56d393cedff62adb8db038f1
madhu-19 <81243444+madhu-19@users.noreply.github.com>
2022-04-18 15:07:06 +0530

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,15 +1,15 @@
 import boto3
-
 import json
 
-s3_client = boto3.client('s3')
+s3_client = boto3.client("s3")
+
 
 def lambda_handler(event, context):
     print(str(event))
-    bucket = event['detail']['requestParameters']['bucketName']
+    bucket = event["detail"]["requestParameters"]["bucketName"]
     print(bucket)
-    json_file_name = event['detail']['requestParameters']['key']
+    json_file_name = event["detail"]["requestParameters"]["key"]
     print(json_file_name)
-    json_object = s3_client.get_object(Bucket=bucket,Key=json_file_name)
-    jsonFileReader = json_object['Body'].read()
-    return 'Hello from Lambda'
+    json_object = s3_client.get_object(Bucket=bucket, Key=json_file_name)
+    jsonFileReader = json_object["Body"].read()
+    return "Hello from Lambda"
