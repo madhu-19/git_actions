d45cbebe462e97569c480d50251f82bf953e4d8c
madhu-19 <81243444+madhu-19@users.noreply.github.com>
2022-04-18 14:55:31 +0530

    Create testcode.py

---
+++ testcode.py
@@ -0,0 +1,14 @@
+import boto3
+import json
+
+s3_client = boto3.client('s3')
+
+  def lambda_handler(event, context):
+    print(str(event))
+    bucket = event['detail']['requestParameters']['bucketName']
+    print(bucket)
+    json_file_name = event['detail']['requestParameters']['key']
+    print(json_file_name)
+    json_object = s3_client.get_object(Bucket=bucket,Key=json_file_name)
+    jsonFileReader = json_object['Body'].read()
+    return 'Hello from Lambda'
